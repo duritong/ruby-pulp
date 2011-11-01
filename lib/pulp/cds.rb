e3e2216404b79608a68c6818c4e745fef034ddd5
mh <mh@immerda.ch>
2011-11-01 19:34:38 +0100

    implement cds correctly

--- lib/pulp/cds.rb
+++ lib/pulp/cds.rb
@@ -3,7 +3,9 @@ module Pulp
     
     has_crud
 
-    pulp_fields :cluster_id, :description, :hostname, :name
+    pulp_fields :cluster_id, :description, :hostname, :name, :sync_schedule
+    
+    pulp_locked_fields :repo_ids, :last_sync , :secret, :hearbeat
 
     pulp_action :associate, :params => true, :returns => Pulp::Task
     pulp_action :un_associate, :params => true, :returns => Pulp::Task
@@ -14,6 +16,10 @@ module Pulp
 
     def history(action)
       self.class.base_get("history/#{action}/",self.id).collect{|th| Pulp::TaskHistory.new(th) }
+    end
+    
+    def repositories
+      repo_ids.collect{|repo_id| Pulp::Repository.find_by_id(repo_id).first }.compact
     end
   end
 end
