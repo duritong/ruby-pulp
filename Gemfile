276bf9162c0c3962abfee49014d6b52b84bffbb2
Markus Krogemann <markus@krogemann.de>
2013-05-16 20:59:26 +0200

    re-engineer build process and make more use of Ruby idioms

--- Gemfile
+++ Gemfile
@@ -3,17 +3,19 @@ source "http://rubygems.org"
 # Example:
 #   gem "activesupport", ">= 2.3.5"
 
-gem "rest-client", "~> 1.6.7"
-gem "activesupport"
-gem "json"
-gem "i18n"
+# gem "rest-client", "~> 1.6.7"
+# gem "activesupport"
+# gem "json"
+# gem "i18n"
 
 # Add dependencies to develop your gem here.
 # Include everything needed to run rake, tests, features, etc.
-group :development do
-  gem "rspec", "~> 2.3.0"
-  gem "bundler", "~> 1.0.0"
-  gem "jeweler", "~> 1.6.4"
-  gem "rcov", ">= 0"
-  gem "mocha"
-end
+# group :development do
+#   gem "rspec", "~> 2.3.0"
+#   gem "bundler", "~> 1.0.0"
+#   gem "jeweler", "~> 1.6.4"
+#   gem "rcov", ">= 0"
+#   gem "mocha"
+# end
+
+gemspec
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,21 +1,25 @@
-$LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
-$LOAD_PATH.unshift(File.dirname(__FILE__))
-require 'rspec'
+# $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
+# $LOAD_PATH.unshift(File.dirname(__FILE__))
+# require 'rspec'
 require 'pulp'
 
 # Requires supporting files with custom matchers and macros, etc,
 # in ./support/ and its subdirectories.
-Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
+# Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
 
 RSpec.configure do |config|
   config.mock_with :mocha
+  config.treat_symbols_as_metadata_keys_with_true_values = true
+  config.run_all_when_everything_filtered = true
+  config.filter_run :focus
+  config.order = 'random'
 end
 
 class DummyResult
   def self.body
     JSON.dump(real_body)
   end
-  
+
   def self.real_body
     { 'a' => 1 }
   end
