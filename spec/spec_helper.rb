4ab45447ee7b23743c085f8fc13462ed4f5161fe
Markus Krogemann <markus@krogemann.de>
2013-05-20 22:27:12 +0200

    make sure simplecov is being used for coverage in Travis

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,11 +1,8 @@
-# $LOAD_PATH.unshift(File.join(File.dirname(__FILE__), '..', 'lib'))
-# $LOAD_PATH.unshift(File.dirname(__FILE__))
-# require 'rspec'
-require 'pulp'
+if ENV['COVERAGE'] == 'true'
+  require 'simplecov'
+end
 
-# Requires supporting files with custom matchers and macros, etc,
-# in ./support/ and its subdirectories.
-# Dir["#{File.dirname(__FILE__)}/support/**/*.rb"].each {|f| require f}
+require 'pulp'
 
 RSpec.configure do |config|
   config.mock_with :mocha
